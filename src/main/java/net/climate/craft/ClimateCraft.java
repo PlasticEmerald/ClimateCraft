9282abe58e4aa549c9ec26af65c8c9cbb323f2e7
Tanner Bart <42682784+PlasticEmerald@users.noreply.github.com>
2019-10-28 20:46:39 -0500

    Setup environment for new mod

---
+++ src/main/java/net/climate/craft/ClimateCraft.java
@@ -0,0 +1,12 @@
+package net.climate.craft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClimateCraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
