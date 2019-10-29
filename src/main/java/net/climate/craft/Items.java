647730fa1867f7c08873f5508806089269505cab
Tanner Bart <42682784+PlasticEmerald@users.noreply.github.com>
2019-10-29 08:55:10 -0500

    Added Item Group

--- src/main/java/net/climate/craft/Items.java
+++ src/main/java/net/climate/craft/Items.java
@@ -1,5 +1,8 @@
 package net.climate.craft;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.item.Item;
@@ -7,11 +10,15 @@ import net.minecraft.item.ItemGroup;
 
 public class Items {
 
+    public static final ItemGroup CLIMATE_GROUP =
+            FabricItemGroupBuilder.build(new Identifier("climatecraft", "items"),
+                    () -> new ItemStack(Blocks.COBBLESTONE));
+
     // List of items
     public static final Item THERMOMETER =
-            register("thermometer", new Item(new Item.Settings().group(ItemGroup.MISC)));
+            register("thermometer", new Item(new Item.Settings().group(Items.CLIMATE_GROUP)));
     public static final Item SOLAR_PANEL =
-            register("solar_panel", new Item(new Item.Settings().group(ItemGroup.MISC)));
+            register("solar_panel", new Item(new Item.Settings().group(Items.CLIMATE_GROUP)));
 
     // Create a new item in the registry
     public static Item register(String name, Item item) {
