3c5778a3aa1038be6503e599ceea2f4c2259bd14
Tanner Bart <42682784+PlasticEmerald@users.noreply.github.com>
2019-11-01 14:50:23 -0500

    Prepared recipes for modification

--- src/main/java/net/climate/craft/blocks/SolarPanelBlock.java
+++ src/main/java/net/climate/craft/blocks/SolarPanelBlock.java
@@ -1,7 +1,6 @@
 package net.climate.craft.blocks;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.sound.BlockSoundGroup;
